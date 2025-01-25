ddc5b66f37b27bf1de1e1639d60619c5022b3013
CorruptedKarma <karmii044@gmail.com>
2025-01-24 22:45:32 -0300

    Overlays :D

--- src/main/java/org/exodusstudio/arcana/client/gui/InteriumMemoriamScreen.java
+++ src/main/java/org/exodusstudio/arcana/client/gui/InteriumMemoriamScreen.java
@@ -1,16 +1,26 @@
 package org.exodusstudio.arcana.client.gui;
 
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.ARGB;
 import org.exodusstudio.arcana.Arcana;
 
-public class InteriumMemoriamScreen extends Screen {
+public class InteriumMemoriamScreen extends Screen  {
+
+    private void renderTextureOverlay(GuiGraphics guiGraphics, ResourceLocation shaderLocation, float alpha) {
+        int i = ARGB.white(alpha);
+        guiGraphics.blit(RenderType::guiTexturedOverlay, shaderLocation, 0, 0, 0.0F, 0.0F, guiGraphics.guiWidth(), guiGraphics.guiHeight(), guiGraphics.guiWidth(), guiGraphics.guiHeight(), i);
+    }
 
     private static final ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath(Arcana.MODID,
             "post_effect/interiormemoriamshader.json");
+    private static final ResourceLocation POWDER_SNOW_OUTLINE_LOCATION = ResourceLocation.withDefaultNamespace("textures/misc/powder_snow_outline.png");
 
 
     public InteriumMemoriamScreen() {
@@ -19,8 +29,14 @@ public class InteriumMemoriamScreen extends Screen {
 
 
     @Override
-    public void render(GuiGraphics Graphics, int mouseX, int mouseY, float partialTicks) {
-        super.render(Graphics, mouseX, mouseY, partialTicks);
+    protected void init() {
+        super.init();
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        renderTextureOverlay(graphics, POWDER_SNOW_OUTLINE_LOCATION, 3f);
+        super.render(graphics, mouseX, mouseY, partialTicks);
     }
 
     @Override
