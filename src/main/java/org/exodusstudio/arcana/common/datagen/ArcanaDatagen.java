76b5c6d403e7a874eb9be9959474aac6991e2c13
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-01-28 15:25:23 -0500

    remove datagen (for now)

--- src/main/java/org/exodusstudio/arcana/common/datagen/ArcanaDatagen.java
+++ src/main/java/org/exodusstudio/arcana/common/datagen/ArcanaDatagen.java
@@ -1,15 +1,9 @@
 package org.exodusstudio.arcana.common.datagen;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.exodusstudio.arcana.Arcana;
-import org.exodusstudio.arcana.common.datagen.provider.ArcanaEnUsGenerator;
-import org.exodusstudio.arcana.common.datagen.provider.ArcanaItemModelProvider;
-
-import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = Arcana.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ArcanaDatagen {
@@ -17,14 +11,14 @@ public class ArcanaDatagen {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent.Client event)
     {
-        //New 1.21.4 data gen function
-        PackOutput output = event.getGenerator().getPackOutput();
+        // TODO : fix this
+        /*PackOutput output = event.getGenerator().getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         event.addProvider(new ArcanaEnUsGenerator(output));
         event.addProvider(new ArcanaItemModelProvider(output));
 
-        /*
+
         var prov = gen.addProvider(event.includeServer(), new ArcanaRegistrySetGenerator(gen.getPackOutput(), event.getLookupProvider())).getRegistryProvider();
         gen.addProvider(event.includeServer(), new ArcanaDataMapGenerator(gen.getPackOutput(), prov));*/
     }
