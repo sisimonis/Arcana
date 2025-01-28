f548e623b6a68b506afc04f13b999fb13ea7644c
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-01-28 11:28:58 -0500

    create the fundamental tag

---
+++ src/main/java/org/exodusstudio/arcana/common/registry/TagRegistry.java
@@ -0,0 +1,11 @@
+package org.exodusstudio.arcana.common.registry;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import org.exodusstudio.arcana.Arcana;
+
+public class TagRegistry {
+    public static final TagKey<Item> FUNDAMENTAL = ItemTags.create(ResourceLocation.fromNamespaceAndPath(Arcana.MODID, "fundamental "));
+}
