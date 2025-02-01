0a8218b05219e76e6da9e19438ce08c7dc1ed6dc
CorruptedKarma <karmii044@gmail.com>
2025-02-01 19:40:01 -0300

    mixins, advancements and locked recipes

--- src/main/java/org/exodusstudio/arcana/common/event/ArcanaModEvents.java
+++ src/main/java/org/exodusstudio/arcana/common/event/ArcanaModEvents.java
@@ -5,18 +5,23 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeManager;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.event.entity.player.AdvancementEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import org.exodusstudio.arcana.Arcana;
 import org.exodusstudio.arcana.client.Keybindings;
 import org.exodusstudio.arcana.client.gui.InteriumMemoriamScreen;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @EventBusSubscriber(modid = Arcana.MODID)
 public class ArcanaModEvents {
