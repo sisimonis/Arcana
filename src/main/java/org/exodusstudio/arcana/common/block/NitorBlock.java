6e0d37e3beaf21df6e5e4b475181938e41c2f149
CorruptedKarma <karmii044@gmail.com>
2025-04-04 18:12:18 -0300

    nitor stuff wip

---
+++ src/main/java/org/exodusstudio/arcana/common/block/NitorBlock.java
@@ -0,0 +1,11 @@
+package org.exodusstudio.arcana.common.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class NitorBlock extends Block {
+
+
+    public NitorBlock(Properties properties) {
+        super(properties);
+    }
+}
